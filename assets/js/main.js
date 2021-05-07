d6cf1445de44a29fb4e40a49af9265c5945445d3
Ulianna0309 <ulianna0309@gmail.com>
2021-05-07 20:00:56 +0300

    2 commits

---
+++ assets/js/main.js
@@ -0,0 +1,101 @@
+function updateTextInput(val) {
+    document.getElementById('textInput').value=val;
+}
+
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+    this.querySelectorAll(".layer").forEach(layer => {
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/100
+        const y = (window.innerHeight - e.pageY*speed)/100
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+$(window).scroll(function(){
+    let scroll = $(window).scrollTop();
+    if(scroll < 50){
+        $('.fixed-top').css('background', 'transparent');
+        $('.navbar-service').css('background', '#f8f6fe');
+    } else{
+        $('.fixed-top').css('background', '#ffffff');
+        $('.navbar-service').css('background', '#ffffff');
+    }
+});
+
+
+
+function setupTypewriter(t) {
+    var HTML = t.innerHTML;
+
+    t.innerHTML = "";
+
+    var cursorPosition = 0,
+        tag = "",
+        writingTag = false,
+        tagOpen = false,
+        typeSpeed = 30,
+        tempTypeSpeed = 0;
+
+    var type = function() {
+
+        if (writingTag === true) {
+            tag += HTML[cursorPosition];
+        }
+
+        if (HTML[cursorPosition] === "<") {
+            tempTypeSpeed = 0;
+            if (tagOpen) {
+                tagOpen = false;
+                writingTag = true;
+            } else {
+                tag = "";
+                tagOpen = true;
+                writingTag = true;
+                tag += HTML[cursorPosition];
+            }
+        }
+        if (!writingTag && tagOpen) {
+            tag.innerHTML += HTML[cursorPosition];
+        }
+        if (!writingTag && !tagOpen) {
+            if (HTML[cursorPosition] === " ") {
+                tempTypeSpeed = 0;
+            }
+            else {
+                tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+            }
+            t.innerHTML += HTML[cursorPosition];
+        }
+        if (writingTag === true && HTML[cursorPosition] === ">") {
+            tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+            writingTag = false;
+            if (tagOpen) {
+                var newSpan = document.createElement("span");
+                t.appendChild(newSpan);
+                newSpan.innerHTML = tag;
+                tag = newSpan.firstChild;
+            }
+        }
+
+        cursorPosition += 1;
+        if (cursorPosition < HTML.length - 1) {
+            setTimeout(type, tempTypeSpeed);
+        }
+
+    };
+
+    return {
+        type: type
+    };
+}
+
+let typewriter = document.getElementById('typewriter');
+
+typewriter = setupTypewriter(typewriter);
+
+typewriter.type();
+
+
